dbca0401e4bac7cbc75fdcf9ffe023f85126fa23
Maicco <maicco.mmg@gmail.com>
2017-05-24 11:55:16 -0300

    Create Main Activity and set the click listener for the TextViews

---
+++ LeagueOfLegends-API/app/src/main/java/com/example/android/leagueoflegends_api/ServerStatusActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.leagueoflegends_api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServerStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_status);
+    }
+}
