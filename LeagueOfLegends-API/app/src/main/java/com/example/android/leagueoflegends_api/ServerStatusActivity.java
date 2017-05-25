62072daf4e317bdff70b42a29c0d5b02cdb85ef1
Maicco <maicco.mmg@gmail.com>
2017-05-25 14:49:28 -0300

    Created the ServerStatusAdapter, ServerStatusLoader and, ServerStatusUtils classes. Up-to-dated the color.xml, strings.xml, styles.xml files. Created the status_circle_xml.xml and server_status_list_item.xml files.

--- LeagueOfLegends-API/app/src/main/java/com/example/android/leagueoflegends_api/ServerStatusActivity.java
+++ LeagueOfLegends-API/app/src/main/java/com/example/android/leagueoflegends_api/ServerStatusActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class ServerStatusActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_status);
     }
